9f5b92fd5ce8797155ac4367234595ed6ee9e3e7
johnson guedes <jondes560@hotmail.com>
2019-11-28 05:38:23 +0000

    Higher order functions
    ES6 Higher order functions

---
+++ JS-Map-Filter-Reduce/map_filter_reduce/main.js
@@ -0,0 +1,15 @@
+
+//Maps method
+const items = [1, 2, 3, 4, 5];
+const doubled =
+    console.log(items.map(x => x * 2));
+
+// Filter method
+const items2 = [1, 2, 3, 4, 5, 6, 7, 8];
+// const even =
+console.log(items2.filter(x => x % 2 + 0));
+
+//Reduce method
+const item3 = [1, 2, 3, 4];
+const sum =
+    console.log(items.reduce((x, acc) => x + acc));
