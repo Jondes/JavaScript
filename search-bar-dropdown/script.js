056e4e63405587a4a8838bd600e3454deb25bd5f
Johnson Guedes <jondes560@hotmail.com>
2024-08-26 09:11:43 +0100

    Create script.js

---
+++ search-bar-dropdown/script.js
@@ -0,0 +1,6 @@
+let profileDropdownList = document.querySelectorAll(".profile-dropdown-list");
+let btn = document.querySelector(".profile-dropdown-btn");
+
+const toggle = () => profileDropdownList.classList.toggle("active");
+
+
